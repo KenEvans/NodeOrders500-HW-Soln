85148366baa664d4ce0e04aeabe0608d21550d92
KenEvans <ken.evans@bellevuecollege.edu>
2021-05-08 16:31:06 -0700

    Update Query1Controller.cs

--- NodeOrders500-HW/Controllers/Query1Controller.cs
+++ NodeOrders500-HW/Controllers/Query1Controller.cs
@@ -9,10 +9,27 @@ namespace NodeOrders500_HW.Controllers
 {
     public class Query1Controller : ApiController
     {
+        /// <summary>
+        /// Get method for Query1 to return which stores are selling CDs for better markups
+        /// Returns store/city name and the count of all CD sales over $13 per store
+        /// Written by Ken Evans
+        /// </summary>
+
         NodeOrders500Entities myDB = new NodeOrders500Entities();
-        public IEnumerable<Order> GetOrders()
+
+        [HttpGet]
+        public IHttpActionResult GetOrders()
         {
-            return myDB.Orders;
+            var bestMarkups = from cdSales in myDB.Orders
+                              where cdSales.pricePaid > 13
+                              group cdSales by cdSales.StoreTable.City into salesCount
+                              orderby salesCount.Count() descending
+                              select new
+                              {
+                                  City = salesCount.Key,
+                                  Count = salesCount.Count()
+                              };
+            return Json(bestMarkups);
         }
     }
 }
