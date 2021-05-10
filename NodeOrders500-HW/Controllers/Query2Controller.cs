f7e89235752a3cd048f839e367b2f3b1a6530219
Khalid <khalidmohamed-j@github.com>
2021-05-09 18:19:58 -0700

    Added query2 controller

---
+++ NodeOrders500-HW/Controllers/Query2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NodeOrders500_HW.Controllers
+{
+    public class Query2Controller : ApiController
+    {
+        NodeOrders500Entities myDB = new NodeOrders500Entities();
+
+        [HttpGet]
+        public IHttpActionResult GetAnnualSales(string lastName)
+        {
+            try
+            {
+                var annualSales = (from salesPerson in myDB.Orders
+                                   where salesPerson.SalesPersonTable.LastName == lastName
+                                   select salesPerson.pricePaid).Sum();
+                return Json(annualSales);
+            }
+            catch (Exception)
+            {
+
+                return Json(0);
+            }
+            
+        }
+    }
+}
---
+++ NodeOrders500-HW/Controllers/Query3Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NodeOrders500_HW.Controllers
+{
+    public class Query3Controller : ApiController
+    {
+        NodeOrders500Entities myDB = new NodeOrders500Entities();
+
+        [HttpGet]
+        public IHttpActionResult GetOrders()
+        {
+            var bestMarkups = from cdSales in myDB.Orders
+                              where cdSales.pricePaid > 13
+                              group cdSales by cdSales.StoreTable.City into salesCount
+                              orderby salesCount.Count() descending
+                              select new
+                              {
+                                  City = salesCount.Key,
+                                  Count = salesCount.Count()
+                              };
+            return Json(bestMarkups);
+        }
+    }
+}
